f23b66e6a793c96025a05fe11ba09aac79436b6b
avp02 <vpetranovskij@gmail.com>
2021-11-11 15:04:41 +0300

    added dependency of logger module in web-app module

--- web-app/src/main/java/com/epam/brest/controller/MotorDepotController.java
+++ web-app/src/main/java/com/epam/brest/controller/MotorDepotController.java
@@ -1,6 +1,5 @@
 package com.epam.brest.controller;
 
-import com.epam.brest.logger.ProjectLogger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +10,6 @@ public class MotorDepotController {
 
     @GetMapping
     public String showMotorDepotPage() {
-        ProjectLogger.log.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         return "motor-depot";
     }
 }
