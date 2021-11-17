6b15f232392bd3e9fb8a28c1edf98c7f169c2fa6
avp02 <vpetranovskij@gmail.com>
2021-11-17 12:27:02 +0300

    changed Queries.java

--- dao-jdbc/src/main/java/com/epam/brest/dao/Queries.java
+++ dao-jdbc/src/main/java/com/epam/brest/dao/Queries.java
@@ -9,11 +9,11 @@ public class Queries {
     public static final String DRIVER_DELETE_BY_ID = "DELETE FROM driver WHERE driver_id=:driverId";
     public static final String DRIVER_FIND_NAME_BY_ID = "SELECT name FROM driver WHERE driver_id=:driverId";
     public static final String DRIVER_FIND_ALL_NAME = "SELECT name FROM driver";
-    public static final String DRIVER_COUNT_CAR = "SELECT d.driver_id, d.name, d.dateStartWork, d.salary, COUNT(c.car_id) as countOfCarsAssignedToDriver FROM driver d LEFT JOIN car c ON d.driver_id=c.driver_id GROUP BY d.driver_id";
+    public static final String DRIVER_COUNT_CAR = "SELECT d.driver_id as driverId, d.name as driverName, d.dateStartWork as driverDateStartWork, d.salary as driverSalary, COUNT(c.car_id) as countOfCarsAssignedToDriver FROM driver d LEFT JOIN car c ON d.driver_id=c.driver_id GROUP BY d.driver_id";
 
     public static final String CAR_FIND_ALL = "SELECT * FROM car";
-    public static final String CAR_FIND_BY_ID = "SELECT * FROM car WHERE car_id=:car_id";
-    public static final String CAR_SAVE ="INSERT INTO car (model, driver_id) VALUES (:model, :driver_id)";
-    public static final String CAR_UPDATE_BY_ID = "UPDATE car SET model=:model, driver_id=:driver_id WHERE car_id=:car_id";
-    public static final String CAR_DELETE_BY_ID = "DELETE FROM car WHERE car_id=:car_id";
+    public static final String CAR_FIND_BY_ID = "SELECT * FROM car WHERE car_id=:carId";
+    public static final String CAR_SAVE ="INSERT INTO car (model, driver_id) VALUES (:carModel, :driverId)";
+    public static final String CAR_UPDATE_BY_ID = "UPDATE car SET model=:carModel, driver_id=:driverId WHERE car_id=:carId";
+    public static final String CAR_DELETE_BY_ID = "DELETE FROM car WHERE car_id=:carId";
 }
